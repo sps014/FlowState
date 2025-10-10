23331a6902b43f1c60650f233fc679155c974d7a
Shivendra P. Singh <shivendrapsingh014@gmail.com>
2025-10-10 23:52:43 +0530

    feat: panning is done via JS now

--- src/FlowState/Components/FlowNodeBase.cs
+++ src/FlowState/Components/FlowNodeBase.cs
@@ -8,10 +8,10 @@ namespace FlowState.Components;
 public abstract class FlowNodeBase : ComponentBase, IDisposable
 {
     [Parameter]
-    public int X { get; set; }
+    public double X { get; set; }
 
     [Parameter]
-    public int Y { get; set; }
+    public double Y { get; set; }
 
     public string Id { get; } = Guid.NewGuid().ToString();
     public abstract ValueTask ExecuteAsync();
